9992712d94e379607bca81193a5e22a8e65c0893
derMunk <homunkulus@hazard-warriors.de>
2012-01-12 13:34:35 +0100

    ssss

--- config2.php
+++ config2.php
@@ -1,3 +1,4 @@
 hier sind ganz geheime zugangsdaten drin :-)
 Noch mehr gehiem zugangdaten aber über gitignore geschützt!!
 Hoffentlich sind die daten jetzt über gitignore geschützt!!!
+Jetzt aber!!! Oder???
