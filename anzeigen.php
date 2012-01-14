ae4546ca6bbda336b288c93c050ab5f58a549a85
derMunk <homunkulus@hazard-warriors.de>
2012-01-14 17:11:15 +0100

    ok

--- anzeigen.php
+++ anzeigen.php
@@ -4,10 +4,10 @@
 <head>
 	<meta charset="utf-8" />
 	<title>Sporttagebuch-2012</title>
-
+    <link rel="stylesheet" href="css/styles.css" />
     <link rel="stylesheet" href="js/jquery-ui-1.8.16.custom/css/pepper-grinder/jquery-ui-1.8.16.custom.css" />
     <link rel="stylesheet" href="js/jqGrid-4.3.1/css/ui.jqgrid.css" />
-    <link rel="stylesheet" href="css/styles.css" />  
+  
     
 	<script type="text/javascript" src="js/jqGrid-4.3.1/js/jquery-1.5.2.min.js"></script>
 	<script type="text/javascript" src="js/xmldom/jquery.xmldom-1.0.min.js"></script> 
@@ -16,16 +16,29 @@
 	<script type="text/javascript" src="js/jquery-ui-1.8.16.custom/js/jquery-ui-1.8.16.custom.min.js"></script>
 	<script type="text/javascript"> 
 					$(document).ready(function(){
-					$('#huhu').jqGrid({
-						url:'functions/read-basis2.php',
+					$('#basis').jqGrid({
+						url:'functions/read-basis.php',
 						datatype: 'xml',
-						colNames:['ID','Datum'],
+						colNames:['ID','Datum','Uhrzeit','KM','Sportart','Laufzeit','Schuhe','AV','MX','TEMP','Strecke','Gewicht','Fett','Bemerkungen'],
 						colModel:[
 							{name:'id_basis',index:'id_basis'},
-							{name:'basis_datum',index:'basis_datum'}],
+							{name:'basis_datum',index:'basis_datum'},
+							{name:'basis_uhrzeit',index:'basis_uhrzeit'},
+							{name:'basis_km',index:'basis_km'},
+							{name:'id_spa',index:'id_spa'},
+							{name:'basis_laufzeit',index:'basis_laufzeit'},
+							{name:'id_schuh',index:'id_schuh'},
+							{name:'basis_av',index:'basis_av'},
+							{name:'basis_mx',index:'basis_mx'},
+							{name:'basis_temp',index:'basis_temp'},
+							{name:'strecke',index:'strecke'},
+							{name:'basis_gewicht',index:'basis_gewicht'},
+							{name:'basis_fett',index:'basis_fett'},
+							{name:'basis_bemerkungen',index:'basis_bemerkungen'}
+							],
 						rowNum:10,
 						autowidth: true,
-						shrinkToFit: false,
+						shrinkToFit: true,
 						cellLayout: 10,
 						height: 'auto',
 						pager: '#pager1',
@@ -35,7 +48,7 @@
 							repeatitems:false
 						}
 
-						
+					
 				  });
 				});					
 				
@@ -44,7 +57,7 @@
 <body>
 
 <br /><h1>Anzeige der STB-Daten</h1>
-<br /><table id="huhu"></table>
+<br /><table id="basis"></table>
 							<div id="pager1"></div>
 							
                             <?php echo $stbTEXT['ltbname']?>
--- functions/read-basis.php
+++ functions/read-basis.php
@@ -8,7 +8,15 @@ $query = sprintf("SELECT * FROM stb_test_basis_daten
 					);
 $result = mysqli_query($db, $query) or die ("what?");
 
-//echo $query;
+
+
+header("Content-type: text/xml;charset=utf-8");
+ 
+echo "<?xml version='1.0' encoding='utf-8'?>";
+echo   "<rows>";
+echo  "<page>".$page."</page>";
+echo "<total>".$total_pages."</total>";
+echo "<records>".$count."</records>";
 while($row = mysqli_fetch_array($result)) {
 
 //echo $row['basis_datum']."<br />";
@@ -16,6 +24,19 @@ while($row = mysqli_fetch_array($result)) {
 	echo "<row id='". $row['id_basis']."'>";			
 	echo "<id_basis><![CDATA[". $row['id_basis']."]]></id_basis>";
 	echo "<basis_datum><![CDATA[". date("d.m.Y",$row['basis_datum'])."]]></basis_datum>";
+	echo "<basis_uhrzeit><![CDATA[". date("d.m.Y",$row['basis_uhrzeit'])."]]></basis_uhrzeit>";
+	echo "<basis_km><![CDATA[". $row['basis_km']."]]></basis_km>";
+	echo "<id_spa><![CDATA[". $row['id_spa']."]]></id_spa>";
+	echo "<basis_laufzeit><![CDATA[". $row['basis_laufzeit']."]]></basis_laufzeit>";
+	echo "<id_schuh><![CDATA[". $row['id_schuh']."]]></id_schuh>";
+	echo "<basis_av><![CDATA[". $row['basis_av']."]]></basis_av>";
+	echo "<basis_mx><![CDATA[". $row['basis_mx']."]]></basis_mx>";
+	echo "<basis_temp><![CDATA[". $row['basis_temp']."]]></basis_temp>";
+	echo "<strecke><![CDATA[". $row['strecke']."]]></strecke>";
+	echo "<basis_gewicht><![CDATA[". $row['basis_gewicht']."]]></basis_gewicht>";
+	echo "<basis_fett><![CDATA[". $row['basis_fett']."]]></basis_fett>";
+	echo "<basis_bemerkungen><![CDATA[". $row['basis_bemerkungen']."]]></basis_bemerkungen>";
+
 	echo "</row>";
 
 }
